cab57d6c5c2e5fc85ee44bd9a79d63555d03ebd3
Gregorick <gregorick.liriano@gmail.com>
2020-05-25 19:44:39 -0400

    Update key.js

--- src/key.js
+++ src/key.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb_URI: {
-        URI: 'mongodb+srv://djcurso:Gr3g0r1ck1234@@cluster0-eg5tm.mongodb.net/test?retryWrites=true&w=majority'
+        URI: 'mongodb+srv://djcurso:<Password>@cluster0-eg5tm.mongodb.net/test?retryWrites=true&w=majority'
     },
     app_config: {
         host: 'http://localhost',
